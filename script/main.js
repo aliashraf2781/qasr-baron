ca579080cbd28f2bcd65e79f3bfce8bd41bfda5e
Ali Ashraf <aaaaaa30101975@gmail.com>
2024-10-09 21:51:37 +0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,16 +1,3 @@
-// Select elements
-const categoryEastern = document.querySelector('.eastern');
-const categoryWestern = document.querySelector('.western');
-const categoryBakery = document.querySelector('.bakery');
-const categoryChocolate = document.querySelector('.chocolates');
-const categoriesContainer = document.querySelector('.categories');
-const categoryButtons = document.querySelectorAll('.category');
-const backButton = document.querySelector('.back-button');
-const bakeryPage = document.querySelector('.bakery-page');
-
 function toPage(page) {
-    window.location.href = `../pages/${page}.html`;
+    window.location.href = `./pages/${page}.html`;
 }
-
-// back button
-
